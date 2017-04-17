771e37f45c4e0d24490db8466de893965679f3be
chrisp9 <chris_powell9@outlook.com>
2017-04-17 12:37:05 +0100

    fixing build

--- build.bat
+++ build.bat
@@ -20,4 +20,4 @@ mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 
-%nuget% pack "ThreadingLib\ThreadingLib.Sdk.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "ThreadingLib\ThreadingLib.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
